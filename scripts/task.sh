9e63564bc012de33c49d1d91f29ce5c62bbdbb80
Robin Bryce <robinbryce@gmail.com>
2022-06-01 22:15:36 +0100

    prefer installed tusk over fetching to local bin

--- scripts/task.sh
+++ scripts/task.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+if which task; then
+  exec $(which task) "$@"
+  exit 1 # execfail defense
+fi
+
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 if [ ! -f $SCRIPT_DIR/../bin/task ]; then
   pushd $SCRIPT_DIR/.. >> /dev/null 2>&1
   sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
   popd >> /dev/null 2>&1
 fi
-$SCRIPT_DIR/../bin/task "$@"
+exec $SCRIPT_DIR/../bin/task "$@"
--- scripts/tusk.sh
+++ scripts/tusk.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+if which tusk; then
+  exec $(which tusk) "$@"
+  exit 1 # execfail defense
+fi
+
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 if [ ! -f $SCRIPT_DIR/../bin/tusk ]; then
   mkdir -p $SCRIPT_DIR/../bin
   curl -sL https://git.io/tusk | bash -s -- -b $SCRIPT_DIR/../bin latest
   chmod a+x $SCRIPT_DIR/../bin/tusk
 fi
-$SCRIPT_DIR/../bin/tusk "$@"
+exec $SCRIPT_DIR/../bin/tusk "$@"
