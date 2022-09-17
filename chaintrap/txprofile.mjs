e175a8e7cf2fd54589a0060f4aa49cb6b670fd5f
Robin Bryce <robinbryce@gmail.com>
2022-09-17 12:50:47 +0100

    transaction profiler

---
+++ chaintrap/txprofile.mjs
@@ -0,0 +1,71 @@
+import { pushFIFO } from "./fifo.mjs"
+
+export class TXProfiler {
+  constructor (movingAverageWindow=10, optional) {
+    this.order = 1
+    this.pending = {}
+    this.movingAverageWindow = movingAverageWindow
+    this.fifo = []
+    this.updated = optional?.updated
+
+    this.now = () => Date.now()
+  }
+
+  async txissue(method, ...args) {
+    const called = this.now()
+    const order = this.order
+    this.order += 1
+
+    const tx = await method(... args)
+
+    this.pending[tx.hash] = {order, tx, called, issued: this.now()}
+    return tx
+  }
+
+  async txwait(tx) {
+    const r = await tx.wait()
+
+    const profiled = this.pending[r.transactionHash]
+    delete this.pending[r.transactionHash]
+    profiled.complete = this.now()
+    profiled.status = r.status == 1
+    profiled.gasUsed = r.gasUsed
+    profiled.gasPrice = r.effectiveGasPrice
+
+    pushFIFO(this.fifo, this.movingAverageWindow, profiled)
+
+    if (this.updated) this.updated(this)
+
+    return r
+  }
+
+  latency() {
+    let sum = 0
+    if (!this.fifo.length) return 0
+
+    this.fifo.forEach(p => sum += (p.complete - p.called))
+    return sum / this.fifo.length
+  }
+
+  gas() {
+    let sum = 0
+    if (!this.fifo.length) return 0
+
+    this.fifo.forEach(p => {
+      if (!p.gasUsed) return
+      sum += p.gasUsed
+    })
+    return sum / this.fifo.length
+  }
+  price () {
+    let sum = 0
+
+    if (!this.fifo.length) return 0
+
+    this.fifo.forEach(p => {
+      if (p.gasPrice) sum += p.gasPrice
+    })
+
+    return sum / this.fifo.length
+  }
+}
