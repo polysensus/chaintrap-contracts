0984756715d7e9c6eb9f6ee960bcc12ddbcb1355
Robin Bryce <robinbryce@gmail.com>
2023-02-19 11:26:36 +0000

    transcript playback hh test passes

--- tests/hh/tests/transcript.mjs
+++ tests/hh/tests/transcript.mjs
@@ -1,11 +1,13 @@
+import chai from 'chai'
+const { expect } = chai
+import hardhat from 'hardhat'
+const { ethers } = hardhat
+const bytes = ethers.utils.arrayify;
+const keccak256 = ethers.utils.keccak256;
+import { deployArena } from "./deploy.js";
+import { createArenaProxy } from './arenaproxy.mjs'
 
-const { expect } = require("chai");
-const { keccak256 } = require("ethers/lib/utils.js");
-const { ethers } = require("hardhat");
-bytes = ethers.utils.arrayify;
-
-// const { NORTH, WEST, SOUTH, EAST } = require("libchaintrap/src/constants.mjs");
-const _chaintrap = import("../../../chaintrap/chaintrap.mjs");
+import { Game, Location, Exit, Link, NORTH, WEST, SOUTH, EAST } from "../../../chaintrap/chaintrap.mjs";
 
 async function locationSides() {
   const chaintrap = await _chaintrap;
@@ -46,222 +48,127 @@ async function locationSides() {
 */
 
 async function loadDefaultMap(arena, gid) {
-  const chaintrap = await _chaintrap;
 
   const locations = [
-    chaintrap.Location.fromHex("0x01", "",           "",           "",               "0x0001").native(),
-    chaintrap.Location.fromHex("0x01", "",           "0x00020005", "0x000600070008", "0x0003").native(),
-    chaintrap.Location.fromHex("0x01", "",           "0x0004",     "",               "").native(),
-    chaintrap.Location.fromHex("0x01", "",           "",           "",               "0x0009").native(),
-    chaintrap.Location.fromHex("0x01", "0x000a",     "",           "",               "0x000b").native(),
-    chaintrap.Location.fromHex("0x01", "0x000d000e", "0x000c",     "",               "").native(),
+    Location.fromHex("0x01", "",           "",           "",               "0x0001").native(),
+    Location.fromHex("0x01", "",           "0x00020005", "0x000600070008", "0x0003").native(),
+    Location.fromHex("0x01", "",           "0x0004",     "",               "").native(),
+    Location.fromHex("0x01", "",           "",           "",               "0x0009").native(),
+    Location.fromHex("0x01", "0x000a",     "",           "",               "0x000b").native(),
+    Location.fromHex("0x01", "0x000d000e", "0x000c",     "",               "").native(),
   ];
 
-  let tx = await arena.loadLocations(gid, locations);
-  let r = await tx.wait();
+  let r = await receipt(arena.loadLocations, gid, locations);
   expect(r.status).to.equal(1);
 
   const exits = [
     // room 1
-    chaintrap.Exit.fromHex("0x00010001").native(), // e1
+    Exit.fromHex("0x00010001").native(), // e1
     // room 2
-    chaintrap.Exit.fromHex("0x00010002").native(), // e2
-    chaintrap.Exit.fromHex("0x00020002").native(), // e3
+    Exit.fromHex("0x00010002").native(), // e2
+    Exit.fromHex("0x00020002").native(), // e3
     // room 3
-    chaintrap.Exit.fromHex("0x00020003").native(), // e4
+    Exit.fromHex("0x00020003").native(), // e4
     // room 2
-    chaintrap.Exit.fromHex("0x00030002").native(), // e5
-    chaintrap.Exit.fromHex("0x00040002").native(), // e6
-    chaintrap.Exit.fromHex("0x00050002").native(), // e7
-    chaintrap.Exit.fromHex("0x00060002").native(), // e8
+    Exit.fromHex("0x00030002").native(), // e5
+    Exit.fromHex("0x00040002").native(), // e6
+    Exit.fromHex("0x00050002").native(), // e7
+    Exit.fromHex("0x00060002").native(), // e8
     // room 4
-    chaintrap.Exit.fromHex("0x00030004").native(), // e9
+    Exit.fromHex("0x00030004").native(), // e9
     // room 5
-    chaintrap.Exit.fromHex("0x00040005").native(), // e10
-    chaintrap.Exit.fromHex("0x00070005").native(), // e11
+    Exit.fromHex("0x00040005").native(), // e10
+    Exit.fromHex("0x00070005").native(), // e11
     // room 6
-    chaintrap.Exit.fromHex("0x00070006").native(), // e12
-    chaintrap.Exit.fromHex("0x00050006").native(), // e13
-    chaintrap.Exit.fromHex("0x00060006").native(), // e14
+    Exit.fromHex("0x00070006").native(), // e12
+    Exit.fromHex("0x00050006").native(), // e13
+    Exit.fromHex("0x00060006").native(), // e14
   ];
-  tx = await arena.loadExits(gid, exits);
-  r = await tx.wait();
+
+  r = await receipt(arena.loadExits, gid, exits);
   expect(r.status).to.equal(1);
 
   const links = [
-    chaintrap.Link.fromHex("0x0100010002").native(), // (1)-(2) ln1
-    chaintrap.Link.fromHex("0x0100030004").native(), // (3)-(4) ln2
-    chaintrap.Link.fromHex("0x0100050009").native(), // (5)-(9) ln3
-    chaintrap.Link.fromHex("0x010006000a").native(), // (6)-(10) ln4
-    chaintrap.Link.fromHex("0x010007000d").native(), // (7)-(13) ln5
-    chaintrap.Link.fromHex("0x010008000e").native(), // (8)-(14) ln6
-    chaintrap.Link.fromHex("0x01000b000c").native() // (11)-(12) ln7
+    Link.fromHex("0x0100010002").native(), // (1)-(2) ln1
+    Link.fromHex("0x0100030004").native(), // (3)-(4) ln2
+    Link.fromHex("0x0100050009").native(), // (5)-(9) ln3
+    Link.fromHex("0x010006000a").native(), // (6)-(10) ln4
+    Link.fromHex("0x010007000d").native(), // (7)-(13) ln5
+    Link.fromHex("0x010008000e").native(), // (8)-(14) ln6
+    Link.fromHex("0x01000b000c").native() // (11)-(12) ln7
   ];
 
-  tx = await arena.loadLinks(gid, links);
-  r = await tx.wait();
+  r = await receipt(arena.loadLinks, gid, links);
   expect(r.status).to.equal(1);
 }
 
-async function createGame() {
-
-    const chaintrap = await _chaintrap;
-
-    const Arena = await ethers.getContractFactory("Arena");
-    let arena = await Arena.deploy();
-    await arena.deployed();
-
-    const [master] = await ethers.getSigners();
-    arena = arena.connect(master)
-    let tx = await arena.createGame(2, "");
-    let r = await tx.wait();
-    const gid = r.events[0].args.gid;
-    return [arena, gid];
-}
-
-class Game {
-
-  static async createDefault(master) {
-    const chaintrap = await _chaintrap;
-
-    const Arena = await ethers.getContractFactory("Arena");
-    let arena = await Arena.deploy();
-    await arena.deployed();
-
-    arena = arena.connect(master);
-
-    let tx = await arena.createGame(2, "");
-    let r = await tx.wait();
-    const gid = r.events[0].args.gid;
-    const tid = r.events[0].args.tid;
-
-    return new Game(new chaintrap.Game(arena, gid, tid));
-  }
-
-  static async connectPlayer(game, player) {
-    const chaintrap = await _chaintrap;
-    return new Game(new chaintrap.Game(game.g.arena.connect(player), game.g.gid, game.g.tid));
-  }
-
-  constructor(game) {
-
-    // Note: game.arena should be connected to the appopriate signer. commitExitUse
-    // will (eventually) revert if its not invoked by the game creator.
-
-    this.g = game;
-  }
-
-  tokenLocation(blocknumber, loc) {
-    return this.g.tokenize(blocknumber, loc);
-  }
-
-  transcriptionLocation(blocknumber, loc) {
-    return this.g.transcriptionLocation(blocknumber, loc);
-  }
-
-  async loadDefaultMap() {
-    return await loadDefaultMap(this.g.arena, this.g.gid, this.g.tid);
-  }
-
-  /**
-   * dev game setup creation & player signup
-   */
-
-  async joinGame(profile) {
-    return this.g.joinGame(profile);
-  }
-
-  async setStartLocation(player, startToken, sceneblob) {
-    return this.g.setStartLocation(player, startToken, sceneblob);
-  }
-
-  /**
-   * 
-   * game phase transitions
-   * @returns 
-   */
-
-  async startGame() {
-    return this.g.startGame();
-  }
-
-  async completeGame() {
-    return this.g.completeGame();
-  }
-
-  /**
-   * game progression
-   */
-
-  async commitExitUse(side, egressIndex)  {
-    return this.g.commitExitUse(side, egressIndex);
-  }
-
-  async allowExitUse(eid, token, scene, side, ingressIndex, halt) {
-    return this.g.allowExitUse(eid, token, scene, side, ingressIndex, halt);
-  }
-
-  /**
-   * game transcript checking & location loading
-   */
-
-  async loadTranscriptLocations(locations) {
-    return this.g.loadTranscriptLocations(locations);
-  }
-
-  async playTranscript() {
-    return this.g.playTranscript();
+function checkStatus(r, msg) {
+  if (r.status != 1) {
+    throw Error(msg || "transaction not successful")
   }
 }
+
+async function newGame(arena, maxPlayers) {
+    let tx = await arena.createGame(maxPlayers, "")
+    let r = await tx.wait()
+    checkStatus(r)
+    return [r.events[1].args.gid, r.events[1].args.tid]
+}
+
+async function receipt(method, ...args) {
+  const tx = await method(...args);
+  return tx.wait();
+}
+
 
 describe("Transcript", function () {
 
+  let proxy;
+  let arena;
+  let masterSigner;
+  let playerSigner;
+  let master, player;
+
+  before(async function () {
+    proxy = await deployArena();
+    const signers = await ethers.getSigners();
+    [masterSigner, playerSigner] = signers;
+    arena = createArenaProxy(proxy.address, masterSigner);
+    master = arena;
+    player = createArenaProxy(proxy.address, playerSigner);
+  });
+
   it("Should load single location without reverting", async function () {
 
-    const chaintrap = await _chaintrap;
+    let r = await receipt(arena.createGame, 2, "");
+    const gid = r.events[1].args.gid;
 
-    const Arena = await ethers.getContractFactory("Arena");
-    const arena = await Arena.deploy();
-    await arena.deployed();
-
-    let tx = await arena.createGame(2, "");
-    let r = await tx.wait();
-    const gid = r.events[0].args.gid;
-
-    tx = await arena.startGame(gid);
-    r = await tx.wait();
+    r = await receipt(arena.startGame, gid);
     expect(r.status).to.equal(1);
 
-    tx = await arena.completeGame(gid);
-    r = await tx.wait();
+    r = await receipt(arena.completeGame, gid);
     expect(r.status).to.equal(1);
 
     const locations = [
-      chaintrap.Location.fromHex("0x01", "", "", "", "0x0001").native()
+      Location.fromHex("0x01", "", "", "", "0x0001").native()
     ];
 
-    tx = await arena.loadLocations(gid, locations);
-    r = await tx.wait();
+    r = await receipt(arena.loadLocations, gid, locations);
+
     expect(r.status).to.equal(1);
   });
 
   it("Should load default map", async function () {
 
-    const Arena = await ethers.getContractFactory("Arena");
-    const arena = await Arena.deploy();
-    await arena.deployed();
+    let r = await receipt(arena.createGame, 2, "");
+    expect(r.status).to.equal(1);
+    const gid = r.events[1].args.gid;
+    const tid = r.events[1].args.tid;
 
-    let tx = await arena.createGame(2, "");
-    let r = await tx.wait();
-    const gid = r.events[0].args.gid;
-    const tid = r.events[0].args.tid;
-
-    tx = await arena.startGame(gid);
-    r = await tx.wait();
+    r = await receipt(arena.startGame, gid);
     expect(r.status).to.equal(1);
 
-    tx = await arena.completeGame(gid);
-    r = await tx.wait();
+    r = await receipt(arena.completeGame, gid);
     expect(r.status).to.equal(1);
 
     await loadDefaultMap(arena, gid, tid);
@@ -269,12 +176,13 @@ describe("Transcript", function () {
 
   it ("Should visit all rooms on the default map", async function() {
 
-    const chaintrap = await _chaintrap;
+    let r = await receipt(arena.createGame, 2, "");
+    expect(r.status).to.equal(1);
+    const gid = r.events[1].args.gid;
+    const tid = r.events[1].args.tid;
 
-    const [master, player] = await ethers.getSigners();
-
-    const gm = await Game.createDefault(master);
-    const gp = await Game.connectPlayer(gm, player);
+    const gm = new Game(master, gid, tid);
+    const gp = new Game(player, gid, tid);
 
     /*
     +------+---------------------------+-----+
@@ -315,9 +223,7 @@ describe("Transcript", function () {
     const locations = [];
     const ids = [];
 
-    const { NORTH, WEST, SOUTH, EAST } = chaintrap.locationSides();
-
-    currentLocationToken = () => {
+    const currentLocationToken = () => {
       return locations[locations.length-1].token;
     }
 
@@ -325,7 +231,7 @@ describe("Transcript", function () {
     locations.push(locationTE(1));
 
     await gp.joinGame("");
-    await gm.setStartLocation(player.address, locations[0].token, []);
+    await gm.setStartLocation(playerSigner.address, locations[0].token, []);
     await gm.startGame();
 
     // In commitAndAllowExit use the player commit to an exit use that takes
@@ -369,7 +275,7 @@ describe("Transcript", function () {
 
     await gm.completeGame();
 
-    await gm.loadDefaultMap();
+    await loadDefaultMap(gm.arena, gid);
     await gm.loadTranscriptLocations(locations);
     await gm.playTranscript();
   });
