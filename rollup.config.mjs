f1f3adf2d75fe104139067b0c7a7c3132ef5762a
Robin Bryce <robin@polysensus.com>
2023-04-05 20:00:20 +0000

    chore: update rollup and vitest

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import pkg from './package.json';
+import pkg from './package.json' assert {type: 'json'};
 
 export default [
 	// browser-friendly UMD build
