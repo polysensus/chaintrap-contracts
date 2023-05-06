0b30d153442313b2e56bdb883e851a4e923858c5
Robin Bryce <robin@polysensus.com>
2023-05-06 08:54:31 +0000

    remove mumbai api key from hardhat config

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -6,41 +6,51 @@
  require("@nomicfoundation/hardhat-toolbox");
  require("hardhat-deploy");
 
- module.exports = {
-   // defaultNetwork: "polygon_mumbai",
-   defaultNetwork: "hardhat",
-   networks: {
+MUMBAI_URL = process.env["MUMBAI_URL"]
+
+let extra_networks = {}
+
+if (MUMBAI_URL) {
+  extra_networks = {...extra_networks, 
      polygon_mumbai: {
-      url: 'https://polygon-mainnet.g.alchemy.com/v2/MGrFH1fAmoFeCc24hxR_vFd9dTmN0DBR',
+      url: MUMBAI_URL,
       accounts: ['0xda36da69010b7baef829d14cf2dfd2caafba98a17aef538161741b2c1992a5a2']
-     },
-     hardhat: {
+     }
+  }
+}
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    ...extra_networks,
+    // always enable hardhat
+    hardhat: {
       allowUnlimitedContractSize: true,
       chainId: 31337,
       gas: "auto",
       gasPrice: 0,
       initialBaseFeePerGas: 0
     }
+ },
+ namedAccounts: {
+   deployer: 10
+ },
+ solidity: {
+    version: "0.8.9",
+    settings: {
+      // hh does not support re-mappings yet
+      optimizer : {
+       enabled: true,
+       runs: 20
+      }
+    }
   },
-  namedAccounts: {
-    deployer: 10
+  paths: {
+    // using hardhat-foundry to get hh to work with foundry & get the benefit
+    // of source remappings. this means using sources here is redundant.
+    // sources: "chaintrap",
+    tests: "./tests/hh/tests",
+    cache: "./build/hh/cache",
+    artifacts: "build/hh/artifacts"
   },
-  solidity: {
-     version: "0.8.9",
-     settings: {
-       // hh does not support re-mappings yet
-       optimizer : {
-        enabled: true,
-        runs: 20
-       }
-     }
-   },
-   paths: {
-     // using hardhat-foundry to get hh to work with foundry & get the benefit
-     // of source remappings. this means using sources here is redundant.
-     // sources: "chaintrap",
-     tests: "./tests/hh/tests",
-     cache: "./build/hh/cache",
-     artifacts: "build/hh/artifacts"
-   },
- };
+};
