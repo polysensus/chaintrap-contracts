0dba6a33d213ffa4df21f5449c2e5cb82bb6abac
Robin Bryce <robin@polysensus.com>
2023-05-29 18:42:02 +0000

    minor fixes

--- lib/interfaces/IGame2Events.sol
+++ lib/interfaces/IGame2Events.sol
@@ -16,18 +16,22 @@ interface IGame2Events {
         uint256 maxParticipants
     );
 
-    event StartLocationSet(
-        uint256 indexed id,
-        address player,
-        bytes32 location,
-        bytes sceneblob
-    );
-
     /// @dev emitted when a participant is registered
     event ParticipantRegistered(
         uint256 indexed id,
         address indexed participant,
         bytes profile
+    );
+
+    /// @dev the choices that were revealed as a consequence of the *previous*
+    /// transcript entry. The eid is 0 when setting the starting choices and
+    /// data.
+    event RevealedChoices(
+        uint256 indexed id,
+        address indexed participant,
+        uint256 eid,
+        bytes32[] choices,
+        bytes data
     );
 
     /// @dev emitted when an act is proposed.
@@ -43,6 +47,7 @@ interface IGame2Events {
         uint256 indexed eid,
         address indexed participant,
         bytes32 rootLabel,
+        bytes32 node,
         bytes data
     );
 
@@ -53,6 +58,7 @@ interface IGame2Events {
         address advocate,
         bytes32 rootLabel,
         LibTranscript.Outcome outcome,
+        bytes32 node,
         bytes data
     );
 
